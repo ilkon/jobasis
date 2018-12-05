9a6d8ddc5320cba838d81efe140e156793a3aad8
Ilya Konyukhov <ilya@konyukhov.com>
2018-12-05 16:17:03 -0500

    React components

--- spec/controllers/posts_controller_spec.rb
+++ spec/controllers/posts_controller_spec.rb
@@ -3,8 +3,8 @@
 require 'rails_helper'
 
 RSpec.describe PostsController, type: :controller do
-  describe "GET #index" do
-    it "returns http success" do
+  describe 'GET #index' do
+    it 'responds with appropriate HTTP code' do
       get :index
       expect(response).to have_http_status(:success)
     end
