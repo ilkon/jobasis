9eacf90ea22288fcd3b5dcc80222c3723e84b36b
Ilya Konyukhov <ilya@konyukhov.com>
2019-02-08 00:18:27 -0500

    Adding JS to bulma

--- app/webpacker/packs/application.js
+++ app/webpacker/packs/application.js
@@ -20,6 +20,9 @@ ActiveStorage.start()
 import { Application } from 'stimulus'
 import { definitionsFromContext } from 'stimulus/webpack-helpers'
 
+//import Bulma from '../javascripts/bulmajs/bulma';
+import Navbar from '../javascripts/bulmajs/plugins/navbar';
+
 const application = Application.start()
 const context = require.context('controllers', true, /\.js$/)
 application.load(definitionsFromContext(context))
