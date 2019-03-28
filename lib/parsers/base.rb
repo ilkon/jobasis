b8c5e2f6fba8cb5bd27dd3e11b82ac217eee5626
Ilya Konyukhov <ilya@konyukhov.com>
2019-03-28 13:55:06 -0400

    Better parsing

---
+++ lib/parsers/base.rb
@@ -0,0 +1,94 @@
+# frozen_string_literal: true
+
+module Parsers
+  class Base
+    class << self
+      # Top-level domains
+      def tld
+        return @tld if @tld
+
+        @tld = []
+        filename = Rails.root.join('lib', 'parsers', 'dict', 'tlds-alpha-by-domain.txt').to_s
+        File.readlines(filename).each do |line|
+          next if line.start_with?('#')
+
+          @tld << line.strip.downcase
+        end
+
+        @tld
+      end
+
+      def plain_emails(text)
+        text.scan(/\b\S+@\S+\.(?:#{tld.join('|')})\b/i)
+      end
+
+      def cryptic_emails(text)
+        text.scan(/\b[\w\.\-+]+\s*(?:@|[\[\(\{]at[\]\)\}])\s*[\w\.\-]+\s*(?:\.|\bdot\b)\s*(?:#{tld.join('|')})\b/i)
+      end
+
+      def domains(text)
+        text.scan(/\b[\w\.\-]+\.(?:#{tld.join('|')})\b/i)
+      end
+
+      def schemed_urls(text)
+        text.scan(%r{\b(?:http[s]?:)?//[\w\.\-]+\.(?:#{tld.join('|')})[\#/\?]?[\w\.\-\+\=\!\#\/\?]*}i)
+      end
+
+      def schemeless_urls(text)
+        text.scan(%r{[\w\.\-]+\.(?:#{tld.join('|')})[\#\/\?]?[\w\.\-\+\=\!\#\/\?]*}i)
+      end
+
+      def paragraphs(text)
+        text.split(%r{<[\\]?\s*p\s*[\/]?>|<br\s*[\/]?>}i).map do |p|
+          AttributeStripper.strip_string(p, collapse_spaces: true, replace_newlines: true, allow_empty: true)
+        end.select(&:present?)
+      end
+
+      def parse_emails(text)
+        mapped = {}
+
+        plain_emails(text).each do |email|
+          pure_email = email.downcase
+          mapped[pure_email] ||= []
+          mapped[pure_email] << email
+        end
+
+        cryptic_emails(text).each do |email|
+          pure_email = email.downcase.gsub(/\bdot\b/i, '.').gsub(/[\[\(\{]at[\]\)\}]/i, '@').delete(' ')
+          mapped[pure_email] ||= []
+          mapped[pure_email] << email
+        end
+
+        pure_emails = mapped.keys.sort_by(&:length).reverse
+
+        pure_emails.each_with_index do |pure_email, i|
+          mapped[pure_email].each do |email|
+            text.gsub!(email, "###EMAIL#{i}###")
+          end
+        end
+
+        pure_emails
+      end
+
+      def parse_urls(text)
+        mapped = {}
+
+        schemed_urls(text).each do |url|
+          pure_url = url.downcase
+          mapped[pure_url] ||= []
+          mapped[pure_url] << url
+        end
+
+        pure_urls = mapped.keys.sort_by(&:length).reverse
+
+        pure_urls.each_with_index do |pure_url, i|
+          mapped[pure_url].each do |url|
+            text.gsub!(url, "###URL#{i}###")
+          end
+        end
+
+        pure_urls
+      end
+    end
+  end
+end
--- lib/parsers/hacker_news.rb
+++ lib/parsers/hacker_news.rb
@@ -3,64 +3,8 @@
 require 'htmlentities'
 
 module Parsers
-  class HackerNews
+  class HackerNews < Base
     class << self
-      def tld
-        return @tld if @tld
-
-        @tld = []
-        filename = Rails.root.join('lib', 'parsers', 'dict', 'tlds-alpha-by-domain.txt').to_s
-        File.readlines(filename).each do |line|
-          next if line.start_with?('#')
-
-          @tld << line.strip.downcase
-        end
-
-        @tld = @tld.sort_by(&:length).reverse
-      end
-
-      def parse_emails(text)
-        emails = text.scan(/[\w\d\.\-+_]+\s*@\s*[\w\d\.\-]+\s*(?:\.|\bdot\b)\s*(?:#{tld.join('|')})\b/i)
-
-        mapped = {}
-        emails.each do |email|
-          pure_email = email.downcase.gsub(/\bdot\b/, '.').delete(' ')
-          mapped[pure_email] ||= []
-          mapped[pure_email] << email
-        end
-
-        pure_emails = mapped.keys.sort_by(&:length).reverse
-
-        pure_emails.each_with_index do |pure_email, i|
-          mapped[pure_email].each do |email|
-            text.gsub!(email, "###EMAIL#{i}###")
-          end
-        end
-
-        pure_emails
-      end
-
-      def parse_urls(text)
-        urls = text.scan(%r{(?:(?:http[s]?:)?//)?[\w\d\.\-]+\.(?:#{tld.join('|')})[^\s<'",]+?\b}i)
-
-        mapped = {}
-        urls.each do |url|
-          pure_url = url.downcase.gsub(/\bdot\b/, '.').delete(' ')
-          mapped[pure_url] ||= []
-          mapped[pure_url] << url
-        end
-
-        pure_urls = mapped.keys.sort_by(&:length).reverse
-
-        pure_urls.each_with_index do |pure_url, i|
-          mapped[pure_url].each do |url|
-            text.gsub!(url, "###URL#{i}###")
-          end
-        end
-
-        pure_urls
-      end
-
       def parse(raw_text)
         parsed = {}
         text = HTMLEntities.new.decode(raw_text)
@@ -75,17 +19,11 @@ module Parsers
         urls = parse_urls(text)
 
         parsed[:text] = text
+        parsed[:paragraphs] = paragraphs(text)
         parsed[:emails] = emails
         parsed[:urls] = urls
 
         parsed
-      end
-
-      def syntax_units(text)
-        _de_p_text = text.split(%r{<[\\]?p>|<br\s*[\/]?>}i).map(&:strip).select(&:present?)
-        # de_p_text.map do |chunk|
-        #   chunk.scan(/[^\.!?]+[\.!?]+|[^\.!?]+.\z/).map(&:strip)
-        # end.flatten
       end
     end
   end
