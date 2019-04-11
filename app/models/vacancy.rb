88f69362b11c4e6e1a7fd83f76c2fbaffd02c754
Ilya Konyukhov <ilya@konyukhov.com>
2019-04-11 12:32:57 -0400

    Beautified output

--- app/models/vacancy.rb
+++ app/models/vacancy.rb
@@ -28,4 +28,26 @@ class Vacancy < ApplicationRecord
       connection.execute("CREATE INDEX #{table}_skill_ids ON #{schema}.#{table} USING GIN (skill_ids jsonb_path_ops)")
     end
   end
+
+  def clean_text(logged_in = false)
+    clean_text = text.dup
+
+    urls.each_with_index do |url, i|
+      clean_text.gsub!("###URL#{i}###", url)
+    end
+
+    if logged_in
+      emails.each_with_index do |email, i|
+        clean_text.gsub!("###EMAIL#{i}###", email)
+      end
+
+    else
+      email_placeholder = "<span class='tag is-placeholder' title='#{I18n.t('vacancies.email_placeholder.title')}'>#{I18n.t('vacancies.email_placeholder.text')}</span>"
+      clean_text.gsub!(/###EMAIL[\d]+###/, email_placeholder)
+    end
+
+    clean_text.gsub!('<a ', '<a rel="nofollow" ')
+
+    clean_text
+  end
 end
