ef55a1a47c8fc38aee17e139da12517ae103167b
Ilya Konyukhov <ilya@konyukhov.com>
2019-05-01 13:25:28 -0400

    Gems update

--- spec/routing/frontend_routing_spec.rb
+++ spec/routing/frontend_routing_spec.rb
@@ -4,4 +4,5 @@ require 'rails_helper'
 
 RSpec.describe 'Frontend', type: :routing do
   it { expect(get: '/').to route_to('vacancies#index') }
+  it { expect(get: '/about').to route_to('pages#about') }
 end
