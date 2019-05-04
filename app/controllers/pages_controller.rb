c7d044a0e9b50c94cf9980e8a247024fabf4b686
Ilya Konyukhov <ilya@konyukhov.com>
2019-05-04 00:00:57 -0400

    Adding data to insights

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,14 @@
 # frozen_string_literal: true
 
 class PagesController < ApplicationController
+  def insights
+    sql = "
+      SELECT S.id, min(S.name) AS name, to_char(V.date, 'YYYY-MM') AS date, count(*) AS vacancies_count
+      FROM #{Skill.table_name} S
+          JOIN #{Vacancy.table_name} V on V.skill_ids @> S.id::text::jsonb
+      GROUP BY S.id, to_char(V.date, 'YYYY-MM')"
+    rows = Vacancy.connection.select_all(sql)
+    @skills = rows.group_by { |r| r['id'] }.values
+    @dates = rows.group_by { |r| r['date'] }.keys.sort
+  end
 end
