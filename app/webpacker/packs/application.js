bffb73679d2007ac378a09786ca790569d066ae4
Ilya Konyukhov <ilya@konyukhov.com>
2021-01-06 00:09:03 -0500

    Stylesheets => application renaming

--- app/webpacker/packs/application.js
+++ app/webpacker/packs/application.js
@@ -7,13 +7,10 @@
 // To reference this file, add <%= javascript_pack_tag 'application' %> to the appropriate
 // layout file, like app/views/layouts/application.html.erb
 
-import Rails from 'rails-ujs'
-import Turbolinks from 'turbolinks'
-import * as ActiveStorage from 'activestorage'
-
-Rails.start()
-Turbolinks.start()
-ActiveStorage.start()
+require('@rails/ujs').start()
+require('turbolinks').start()
+require('@rails/activestorage').start()
+require('channels')
 
 import {Application} from 'stimulus'
 import {definitionsFromContext} from 'stimulus/webpack-helpers'
