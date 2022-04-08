89ff5b74a171d8efe1ede9e26ac6ce83a63903ca
Ilya Konyukhov <ilya@konyukhov.com>
2022-04-07 22:10:31 -0400

    Adding D3 JS lib

--- config/importmap.rb
+++ config/importmap.rb
@@ -6,7 +6,7 @@ pin 'application'
 pin '@hotwired/turbo-rails', to: 'turbo.min.js'
 pin '@hotwired/stimulus', to: 'stimulus.min.js'
 pin '@hotwired/stimulus-loading', to: 'stimulus-loading.js'
-pin 'd3', to: 'd3.js'
+pin 'd3', to: 'd3.js', preload: true
 
 pin_all_from 'app/javascript/controllers', under: 'controllers'
 pin_all_from 'app/javascript/bulmajs', under: 'bulmajs'
